eb3c81af4692866683a81b72118333ec0214c8b5
Victor Ioan Gutoi <victor.ioan.gutoi@gmail.com>
2024-02-17 15:52:20 +0100

    Initial commit

---
+++ src/main/java/net/sandyosan/legacycrafting/LegacyCrafting.java
@@ -0,0 +1,14 @@
+package net.sandyosan.legacycrafting;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LegacyCrafting implements ModInitializer {
+	public static final String MOD_ID = "legacycrafting";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/sandyosan/legacycrafting/LegacyCraftingModClient.java
@@ -0,0 +1,10 @@
+package net.sandyosan.legacycrafting;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LegacyCraftingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
